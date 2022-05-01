6b74e2be822aea9e2d8a3701549cfb5c8e9d8505
Sahil Gupta <68184542+Sahil2012@users.noreply.github.com>
2022-05-01 14:49:49 +0530

    final TextUtils 2

--- src/Components/TextBody.js
+++ src/Components/TextBody.js
@@ -6,14 +6,13 @@ import { useState } from 'react';
 export default function TextBody(props) {
 
     let [text, setText] = useState('');
-    let [valText, setValText] = useState('');
 
     let onTextChange = (event) => {
-        setValText(event.target.value);
+        setText(event.target.value);
     }
 
     let onClickUp = () => {
-        let newText = valText.trim().toUpperCase();
+        let newText = text.trim().toUpperCase();
 
         if (newText === "") {
             props.showAlert("Enter text to perform operation", "Danger");
@@ -26,7 +25,7 @@ export default function TextBody(props) {
     }
 
     let onClickLow = () => {
-        let newText = valText.trim().toLowerCase();
+        let newText = text.trim().toLowerCase();
 
         if (newText === "") {
             props.showAlert("Enter text to perform operation", "Danger");
@@ -39,17 +38,16 @@ export default function TextBody(props) {
     }
 
     let clearText = () => {
-        setValText("");
         setText("");
     }
 
     let onClickCopy = () => {
 
-        if (valText.trim() === "") {
+        if (text.trim() === "") {
             props.showAlert("Enter text to perform operation", "Danger");
 
         } else {
-            navigator.clipboard.writeText(valText);
+            navigator.clipboard.writeText(text);
             props.showAlert("Copied to Clipboard", "Success");
         }
 
@@ -57,10 +55,10 @@ export default function TextBody(props) {
 
     let onClickRemove = () => {
 
-        if (valText.trim() === "") {
+        if (text.trim() === "") {
             props.showAlert("Enter text to perform operation", "Danger");
         } else {
-            let listWords = valText.split(/[ ]+/);
+            let listWords = text.split(/[ ]+/);
             setText(listWords.join(" "));
             props.showAlert("Whitespaces removed", "Success");
         }
@@ -72,22 +70,20 @@ export default function TextBody(props) {
             <div style={{ color: props.mode === 'light' ? 'black' : 'white' }}>
                 <h1>{props.heading}</h1>
                 <div className="mb-3">
-                    <textarea className="form-control" style={{ backgroundColor: props.mode === 'dark' ? '#c9ccd9' : 'white' }} value={valText} onChange={onTextChange} id="exampleFormControlTextarea1" rows="9"></textarea>
+                    <textarea className="form-control" style={{ backgroundColor: props.mode === 'dark' ? '#c9ccd9' : 'white' }} value={text} onChange={onTextChange} id="exampleFormControlTextarea1" rows="9"></textarea>
                 </div>
 
-                <button type="button" className="btn btn-success mx-2" onClick={onClickUp}>Make Uppercase</button>
-                <button type="button" className="btn btn-success mx-2" onClick={onClickLow}>Make LowerCase</button>
-                <button type="button" className="btn btn-success mx-2" onClick={onClickRemove}>Remove Space</button>
-                <button type="button" className="btn btn-warning mx-2" onClick={onClickCopy}>Copy Text</button>
-                <button type="button" className="btn btn-danger mx-2" onClick={clearText}>Clear</button>
+                <button type="button" className="btn btn-success mx-2 my-2" onClick={onClickUp}>Make Uppercase</button>
+                <button type="button" className="btn btn-success mx-2 my-2" onClick={onClickLow}>Make LowerCase</button>
+                <button type="button" className="btn btn-success mx-2 my-2" onClick={onClickRemove}>Remove Space</button>
+                <button type="button" className="btn btn-warning mx-2 my-2" onClick={onClickCopy}>Copy Text</button>
+                <button type="button" className="btn btn-danger mx-2 my-2" onClick={clearText}>Clear</button>
             </div>
 
             <div className='container my-2' style={{ color: props.mode === 'light' ? 'black' : 'white' }}>
                 <h3>Summary about your text</h3>
-                <p>Your text has {valText.trim().length === 0 ? 0 : valText.trim().split(" ").length} words and {valText.trim().length} characters</p>
-                <p>Time to read : {0.08 * (valText.trim().length === 0 ? 0 : valText.trim().split(" ").length)}s</p>
-                <h3>Answer : </h3>
-                <p>{text}</p>
+                <p>Your text has {text.trim().length === 0 ? 0 : text.trim().split(" ").length} words and {text.trim().length} characters</p>
+                <p>Time to read : {0.08 * (text.trim().length === 0 ? 0 : text.trim().split(" ").length)}s</p>
             </div>
         </>
     )
